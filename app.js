4d8ccf8242730c49e94bb3c17eb24123b89bf25d
Rian Jasmine (RJ) <rjquiban@gmail.com>
2020-08-12 20:30:44 -0700

    Responsive navbar

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var myFullpage = new fullpage('#fullpage', {
     scrollBar: true,
-    anchors: ['home', 'about', 'experience', 'projects'],
+    anchors: ["home", "about", "experience", "projects"],
+    menu: "#menu",
 
-    afterLoad: function(origin, destination, direction) {
-        if(destination.anchor == 'about') {
+    afterRender: () => {
+        // selectors for Floop card
+        let floopCard = document.querySelector("#floopCard");
+        let floopFront = document.querySelector("#floopCard .front");
+        let floopBack = document.querySelector("#floopCard .back");
 
-        }
+        // selectors for Microsoft card
+        let msCard = document.querySelector("#msCard");
+        let msFront = document.querySelector("#msCard .front");
+        let msBack = document.querySelector("#msCard .back");
+
+        $(floopCard).mouseup( () => {
+            $(floopFront).toggleClass("hide");
+            $(floopBack).toggleClass("hide");
+        });
+
+        $(msCard).mouseup( () => {
+            $(msFront).toggleClass("hide");
+            $(msBack).toggleClass("hide");
+        });
     }
 });

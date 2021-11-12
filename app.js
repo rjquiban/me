f70c73295f0a32e3213fa07a7993de824e2c4c15
rjquiban <rjquiban@gmail.com>
2021-11-12 15:36:20 -0800

    update experience and profile

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var myFullpage = new fullpage('#fullpage', {
     menu: "#menu",
 
     afterRender: () => {
+        // selectors for PTHub card
+        let pthubCard = document.querySelector("#pthubCard");
+        let pthubFront = document.querySelector("#pthubCard .front");
+        let pthubBack = document.querySelector("#pthubCard .back");
+
         // selectors for Floop card
         let floopCard = document.querySelector("#floopCard");
         let floopFront = document.querySelector("#floopCard .front");
@@ -13,6 +18,11 @@ var myFullpage = new fullpage('#fullpage', {
         let msCard = document.querySelector("#msCard");
         let msFront = document.querySelector("#msCard .front");
         let msBack = document.querySelector("#msCard .back");
+
+        $(pthubCard).mouseup( () => {
+            $(pthubFront).toggleClass("hide");
+            $(pthubBack).toggleClass("hide");
+        });
 
         $(floopCard).mouseup( () => {
             $(floopFront).toggleClass("hide");
